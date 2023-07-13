3a6bc6b0af998aa667e42b694982d80fa4b9a861
luisangeluis <luisangeluis@gmail.com>
2023-07-12 23:22:01 -0600

    Adding post method

--- src/redux/features/counterSlice.ts
+++ src/redux/features/counterSlice.ts
@@ -1,8 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  counter: 0
-}
+  counter: 0,
+};
 
 export const counterSlice = createSlice({
   name: "counter",
@@ -10,15 +10,15 @@ export const counterSlice = createSlice({
   reducers: {
     setCounter: (state, action) => action.payload,
     incrementCounter: (state) => {
-      state.counter += 1
+      state.counter += 1;
     },
     decrementCounter: (state) => {
-      state.counter -= 1
-    }
-  }
+      state.counter -= 1;
+    },
+  },
+});
 
-})
-
-export const { setCounter, incrementCounter, decrementCounter } = counterSlice.actions;
+export const { setCounter, incrementCounter, decrementCounter } =
+  counterSlice.actions;
 
 export default counterSlice.reducer;
--- src/redux/services/userApi.ts
+++ src/redux/services/userApi.ts
@@ -1,25 +1,34 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 type User = {
-  id: number,
-  name: string,
-  email: string,
-  username: string
-}
+  id: number;
+  name: string;
+  email: string;
+  username: string;
+};
 
 export const userApi = createApi({
   reducerPath: "userAPI",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://jsonplaceholder.typicode.com/",
   }),
+  tagTypes:["User"],
   endpoints: (builder) => ({
     getUsers: builder.query<User[], null>({
-      query: () => "users"
+      query: () => "users",
     }),
     getUserById: builder.query<User, { id: string }>({
-      query: ({ id }) => `users/${id}`
+      query: ({ id }) => `users/${id}`,
+    }),
+    addPost: builder.mutation<User, Partial<User>>({
+      query: (body) => ({
+        url: `users`,
+        method: 'POST',
+        body,
+      }),
+      invalidatesTags: [{ type: 'User', id: 'LIST' }],
     })
-  })
-})
+  }),
+});
 
-export const { useGetUsersQuery, useGetUserByIdQuery } = userApi
+export const { useGetUsersQuery, useGetUserByIdQuery } = userApi;
