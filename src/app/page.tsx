47c89c1c7c9ea0107bf3901443724f8ed3be4b41
luis gonzález <65631946+luisangeluis@users.noreply.github.com>
2023-08-07 23:42:42 +0000

    Adding endpoint to get an user by id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,87 @@
-"use client"
+"use client";
 
-import { useAppDispatch, useAppSelector } from '@/redux/hooks'
-import { decrementCounter, incrementCounter } from '@/redux/features/counterSlice';
-import { useGetUsersQuery } from '@/redux/services/userApi';
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import {
+  decrementCounter,
+  incrementCounter,
+} from "@/redux/features/counterSlice";
+import {
+  useGetUserByIdQuery,
+  useGetUsersQuery,
+} from "@/redux/services/userApi";
+import { useState } from "react";
 
 const HomePage = () => {
-  const count = useAppSelector(state => state.counterReducer.counter);
+  const count = useAppSelector((state) => state.counterReducer.counter);
   const dispatch = useAppDispatch();
-  const { data, error, isLoading, isFetching } = useGetUsersQuery(null);
+  const [id, setId] = useState<string | null>(null);
 
-  if (isLoading || isFetching) return <p>Loading...</p>
-  if (error) return <p>Error</p>
+  const {
+    data: users,
+    error: usersError,
+    isLoading: isLoadingUsers,
+    isFetching: isFetchingUsers,
+  } = useGetUsersQuery(null);
+  const {
+    data: user,
+    error: userError,
+    isLoading: isLoadingUser,
+    isFetching: isFetchingUser,
+  } = useGetUserByIdQuery({ id }, { skip: !id });
+
+  if (isLoadingUsers || isFetchingUsers) return <p>Loading...</p>;
+  if (usersError) return <p>usersError</p>;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target as HTMLFormElement);
+    // const id = parseInt(formData.get("id") as string, 10);
+    const id = formData.get("id")?.toString();
+
+    if (id) setId(id);
+  };
 
   return (
     <section>
-      <h1 className='text-center text-2xl'>Count: {count}</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="id">Introduce el id</label>
+        <br></br>
+        <input type="text" id="id" name="id" />
+        <button type="submit">Click</button>
+      </form>
+      <section>
+        <h2>User: {user ? user?.name : userError?.message}</h2>
+      </section>
+      <h1 className="text-center text-2xl">Count: {count}</h1>
       <div className="buttonSection flex justify-center gap-2">
-        <button className='bg-green-500 py-3 px-2 rounded-md' onClick={() => dispatch(incrementCounter())}>Increment</button>
-        <button className='bg-blue-500 py-3 px-2 rounded-md' onClick={() => dispatch(decrementCounter())}>Decrement</button>
+        <button
+          className="bg-green-500 py-3 px-2 rounded-md"
+          onClick={() => dispatch(incrementCounter())}
+        >
+          Increment
+        </button>
+        <button
+          className="bg-blue-500 py-3 px-2 rounded-md"
+          onClick={() => dispatch(decrementCounter())}
+        >
+          Decrement
+        </button>
       </div>
-      <div className='grid grid-cols-3 gap-2 p-2'>
-        {
-          data?.map((user, id) => {
-            return <div key={id} className='bg-slate-200'>
+      <div className="grid grid-cols-3 gap-2 p-2">
+        {users?.map((user, id) => {
+          return (
+            <div key={id} className="bg-slate-200">
               <p>{user.id}</p>
               <p>{user.name}</p>
               <p>{user.email}</p>
               <p>{user.username}</p>
             </div>
-          })
-        }
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/redux/services/userApi.ts
+++ src/redux/services/userApi.ts
@@ -7,27 +7,31 @@ type User = {
   username: string;
 };
 
+interface GetUserQuery {
+  id: string | null;
+}
+
 export const userApi = createApi({
   reducerPath: "userAPI",
   baseQuery: fetchBaseQuery({
     baseUrl: "https://jsonplaceholder.typicode.com/",
   }),
-  tagTypes:["User"],
+  tagTypes: ["User"],
   endpoints: (builder) => ({
     getUsers: builder.query<User[], null>({
       query: () => "users",
     }),
-    getUserById: builder.query<User, { id: string }>({
+    getUserById: builder.query<User, GetUserQuery>({
       query: ({ id }) => `users/${id}`,
     }),
     addPost: builder.mutation<User, Partial<User>>({
       query: (body) => ({
         url: `users`,
-        method: 'POST',
+        method: "POST",
         body,
       }),
-      invalidatesTags: [{ type: 'User', id: 'LIST' }],
-    })
+      invalidatesTags: [{ type: "User", id: "LIST" }],
+    }),
   }),
 });
 
